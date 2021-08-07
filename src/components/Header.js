4a209656a418c1e527bde7fa82fda9e663d8339f
sanjana <sanjanasahoo7@gmail.com>
2021-08-07 14:38:07 +0530

    fixed header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,13 +2,13 @@ import { Affix,PageHeader } from 'antd';
 
 export default function Header() {
     return (
-        <Affix  className="header" >
+        <div className="header">
             <PageHeader
                 id="heading"
                 title="Home"
                 subTitle="Welcome to Todoist"
 
             />
-        </Affix>
+        </div>
     )
 }
